6c2ff65c9ffbcd34f541181ef87111c41343edd4
RyonDhimas <dhimasryon@gmail.com>
2024-04-06 19:40:59 +0800

    Memanggil method dari Class di package lain

---
+++ model/Dosen.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Dosen{
+ public static void tampilaninfo(){
+  System.out.println();
+  System.out.println("========== Data Dosen ===========");
+  System.out.println("NPM : 2210010547");
+  System.out.println("Nama : Ryon Dhimas Haldhi Saputra"); 
+  System.out.println("=================================");
+ }
+}
